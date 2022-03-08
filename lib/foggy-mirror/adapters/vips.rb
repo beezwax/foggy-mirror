a8ba2858edffbab7623a6cc15c1fb21800ab8795
Pedro Fayolle <pfayolle@gmail.com>
2022-03-08 14:26:52 +0900

    Fix crash with some images when using Vips

--- lib/foggy-mirror/adapters/vips.rb
+++ lib/foggy-mirror/adapters/vips.rb
@@ -10,39 +10,36 @@ module FoggyMirror
     end
 
     def dominant_color
-      html_color *color_avg(@image || image(DEFAULT_RESOLUTION))
+      html_color *color_avg(load_image(DEFAULT_RESOLUTION))
     end
 
     def color_samples(res)
-      image(res).to_a.flatten(1).map { |rgb| html_color *rgb }
+      square_image(res).to_a.flatten(1).map { |rgb| html_color *rgb }
     end
 
     private
 
-    def image(res = nil)
-      if @image && res && @image.width != res
-        @image = nil
-      end
-
+    def square_image(res)
       # We load the image as a thumbnail, shrinking it to double the resolution
       # requested, making use of libvips' shrink-on-load optimization, which
       # should be way faster than loading the entire image and shrinking.
       #
       # This optimization assumes the original image isn't more than twice as
-      # wide as high, or vice-versa.
-      @image ||= 
-        begin
-          im = ::Vips::Image.thumbnail(@path, res * 2)
+      # wide as it is high, or vice-versa.
+      im = load_image(res * 2)
 
-          # Make the image square
-          if im.width > im.height
-            im = im.reduceh(im.width.to_f / im.height)
-          elsif im.height > im.width
-            im = im.reducev(im.height.to_f / im.width)
-          end
+      # Make the image square
+      if im.width > im.height
+        im = im.reduceh(im.width.to_f / im.height)
+      elsif im.height > im.width
+        im = im.reducev(im.height.to_f / im.width)
+      end
 
-          im.resize(res.to_f / im.width)
-        end
+      im.resize(res.to_f / im.width)
+    end
+
+    def load_image(res)
+      ::Vips::Image.thumbnail(@path, res)
     end
 
     def color_avg(image)
